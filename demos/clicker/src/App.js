d6ae1644e01a15c3a00781f75d2e85e9b3c19a58
jeanetteobr <jeanette.obr@gmail.com>
2021-04-13 13:55:36 -0400

    Convert App component in clicker app to function component.

--- demos/clicker/src/App.js
+++ demos/clicker/src/App.js
@@ -1,36 +1,67 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      clicks: 0
+// Moved export default keywords to top of
+// function component definition
+export default function App() {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     clicks: 0
+  //   }
+  // }
+
+  // Lines 5-10 are replated with the useState()
+  // React hook!
+  const [clickText, setClickText] = useState(0);
+
+  // clickText () {
+  //   if (this.state.clicks === 1) {
+  //     return `Clicked 1 time `
+  //   }
+  //   return `Clicked ${this.state.clicks} times`
+  // }
+
+  // lines 16-21 are replaced with an arrow function
+  // instead of a method definition
+  const handleClickText = () => {
+    if (clickText === 1) {
+      return <p>Clicked {clickText} time</p>;
     }
-  }
+    return <p>Clicked {clickText} times</p>;
+  };
 
-  clickText () {
-    if (this.state.clicks === 1) {
-      return `Clicked 1 time `
-    }
-    return `Clicked ${this.state.clicks} times`
-  }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <main>
+  //         <h2>Clicker App</h2>
+  //         <p>{this.clickText()}</p>
+  //         <button
+  //           onClick={event => this.setState({ clicks: this.state.clicks + 1 })}
+  //           >
+  //             Click me!
+  //           </button>
+  //       </main>
+  //     </div>
+  //   );
+  // }
 
-  render() {
-    return (
-      <div className="App">
-        <main>
-          <h2>Clicker App</h2>
-          <p>{this.clickText()}</p>
-          <button 
-            onClick={event => this.setState({ clicks: this.state.clicks + 1 })}
-            >
-              Click me!
-            </button>
-        </main>
-      </div>
-    );
-  }
+  // lines 34-38 are replaced with a return because we
+  // converted our component to a function and no londer
+  // need a render method
+  return (
+    <div className="App">
+      <main>
+        <h2>Clicker App</h2>
+        {/* <p>{this.clickText()}</p> */}
+        {/* Lin 56 is replaced with a function call to render correct count */}
+        {handleClickText()}
+        <button onClick={() => setClickText(clickText + 1)}>Click me!</button>
+      </main>
+    </div>
+  );
 }
 
-export default App;
+// export default App; <--- this now lives at the beginning
+// of our fucntion definition
